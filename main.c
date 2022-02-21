8fd3f4446b6e5ca5d9bb0a3eb265c2badbfdec2f
Guillaume dse <guillaume.desaintetienne@edu.ece.fr>
2022-02-21 21:35:05 +0100

    test

---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by Guillaume dse on 21/02/2022.
+//
+
+#include "main.h"
+
+void main(){
+    printf("hello world !")
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Guillaume dse on 21/02/2022.
+//
+
+#ifndef JEU1_MAIN_H
+#define JEU1_MAIN_H
+
+#endif //JEU1_MAIN_H

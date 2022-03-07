68a479d470f5f049bc4715b2d933ccf471a2c222
Guillaume dse <guillaume.desaintetienne@edu.ece.fr>
2022-03-07 17:21:22 +0100

    arret programme

--- jeu.c
+++ jeu.c
@@ -4,23 +4,23 @@
 
 #include "main.h"
 
-int nombreAleatoireEntre(int min, int max){
-    return rand() %(max- min+1 )+min;
+int nombreAleatoireEntre(int min, int max) {
+    return rand() % (max - min + 1) + min;
 }
 
-void choixNombreDeVie(int* vie){
-    do{
+void choixNombreDeVie(int *vie) {
+    do {
         printf("Combien de vie voulez-vous (entre 5 et 10)\n");
         scanf("%d", vie);
-    }while(*vie < 5 || *vie > 10);
+    } while (*vie < 5 || *vie > 10);
 }
 
-void choixDuJ(int* nombreDejoueur, int* choixJ){
+void choixDuJ(int *nombreDejoueur, int *choixJ) {
     *choixJ = nombreAleatoireEntre(1, *nombreDejoueur);
     printf("Le joueur %d doit choisir le mot !\n", *choixJ);
 }
 
-void setup(int* nombreDejoueur, int* choixJ, int* vie){
+void setup(int *nombreDejoueur, int *choixJ, int *vie) {
     choixDuJ(nombreDejoueur, choixJ);
     choixNombreDeVie(vie);
 
@@ -30,8 +30,8 @@ void setup(int* nombreDejoueur, int* choixJ, int* vie){
 }
 
 
-bool verificationLettre(char* mot, char* affichageMot, char lettre, int* position) {
-    for (int i = 0; i<=strlen(mot); i++){
+bool verificationLettre(char *mot, char *affichageMot, char lettre, int *position) {
+    for (int i = 0; i <= strlen(mot); i++) {
         if (lettre == mot[i] && affichageMot[i] == '_') {
             *position = i;
             return true;
@@ -41,11 +41,11 @@ bool verificationLettre(char* mot, char* affichageMot, char lettre, int* positio
     return false;
 }
 
-void affichage(char* mot, int vie){
+void affichage(char *mot, int vie) {
     printf("%s\nIl vous reste %d vie(s) !\n", mot, vie);
 }
 
-int jeu( int NombreDeVie, Mot mot,int vie,int position){
+int jeu(int NombreDeVie, Mot mot, int vie, int position) {
     printf("Choisir le mot :\n");
     gets(mot.mot);
     choixNombreDeVie(&vie);
@@ -55,44 +55,44 @@ int jeu( int NombreDeVie, Mot mot,int vie,int position){
     bool reponse = false, reponseTrouve = false;
     bool vieRecu = false;
     int rep = 0;
-    for(int a = 0; a < strlen(mot.mot); a++){
+    for (int a = 0; a < strlen(mot.mot); a++) {
         motAffiche[a] = '_';
     }
-    do{
-        do{
-            printf("choisissez une lettre\n");
-            fflush(stdin);
-            gets(&lettre);
-            fflush(stdin);
-            reponse = verificationLettre(mot.mot, motAffiche,lettre, &position);
 
-            if (reponse == 1){
-                motAffiche[position] = lettre;
-                }
+    do {
+        printf("choisissez une lettre\n");
+        fflush(stdin);
+        gets(&lettre);
+        fflush(stdin);
+        reponse = verificationLettre(mot.mot, motAffiche, lettre, &position);
 
-            if (reponse == 0){
-                vie -=1;
-                if (vie ==0){
-                    printf("you are a looser!\n");
-                    printf("le mot etait %s !!!",mot.mot);
-                    return 0;
-                }
+        if (reponse == 1) {
+            motAffiche[position] = lettre;
+        }
+
+        if (reponse == 0) {
+            vie -= 1;
+            if (vie == 0) {
+                printf("you are a looser!\n");
+                printf("le mot etait %s !!!", mot.mot);
+                return 0;
             }
-            for (int y = 0; y < strlen(mot.mot); y++){
-                rep = 0;
-                if (mot.mot[y]==motAffiche[y])
-                    rep++;
-            }
-            if (rep == strlen(mot.mot))
-                reponseTrouve = true;
+        }
+        for (int y = 0; y < strlen(mot.mot); y++) {
+            rep = 0;
+            if (mot.mot[y] == motAffiche[y])
+                rep++;
+        }
+        if (rep == strlen(mot.mot))
+            reponseTrouve = true;
 
-            if (reponseTrouve == true){
-                printf("bravo vous avez trouve !");
-                printf("il vous restait %d vie(s).",vie);
-            }
-            affichage(motAffiche, vie);
+        if (reponseTrouve == true) {
+            printf("bravo vous avez trouve !");
+            printf("il vous restait %d vie(s).", vie);
+        }
+        affichage(motAffiche, vie);
 
-        }while (reponse == false && vie > 0);
+    } while ((reponse == false && vie > 0) || reponseTrouve == false);
 
-    }while (vie != 0 || reponseTrouve==false);
+
 }

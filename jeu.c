2bf3e3bfd9204b883d9e48d5be50390fb1dec507
emilien <emilien.godet@edu.ece.fr>
2022-03-10 14:47:17 +0100

    version9

--- jeu.c
+++ jeu.c
@@ -3,7 +3,7 @@
 //
 
 #include "main.h"
-/*
+
 int menu1() {
     char choix = 0;
     bool erreur;
@@ -104,37 +104,35 @@ void affichage(char *mot, int vie) {
     printf("%s\nIl vous reste %d vie(s) !\n", mot, vie);
 }
 
-void affichageAllegro(int *vie){
-    assert(al_init());
+void imagePendu(){
     ALLEGRO_DISPLAY *display = NULL;
+    assert(al_init());
     display = al_create_display(800, 600);
     assert(display != NULL);
     al_set_window_title(display, "JEU DE PENDU");
-    al_clear_to_color(al_map_rgb(255, 255,  255));
-    al_flip_display();
+    al_init_primitives_addon();
+    al_clear_to_color(al_map_rgb(255, 20,  20));
     al_draw_line(100, 550, 700, 550, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
     al_draw_line(150, 550, 150, 50, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
-    al_draw_line(100, 50, 500, 50, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
+    al_draw_line(150, 130, 230, 50, al_map_rgb(0, 0, 0),5);
+    al_draw_line(130, 50, 430, 50, al_map_rgb(0, 0, 0),5);
     al_draw_line(400, 50, 400, 70, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
     al_draw_circle(400,100,30,al_map_rgb(0, 0, 0),5);
-    al_flip_display();
-    al_draw_line(400, 130, 400, 350, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
-    al_draw_line(400, 350, 450, 450, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
-    al_draw_line(400, 350, 350, 450, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
+    al_draw_line(400, 130, 400, 280, al_map_rgb(0, 0, 0),5);
+    al_draw_line(400, 280, 450, 370, al_map_rgb(0, 0, 0),5);
+    al_draw_line(400, 280, 350, 370, al_map_rgb(0, 0, 0),5);
     al_draw_line(400, 180, 350, 150, al_map_rgb(0, 0, 0),5);
-    al_flip_display();
     al_draw_line(400, 180, 450, 150, al_map_rgb(0, 0, 0),5);
+    al_draw_line(385, 85, 395, 95, al_map_rgb(0, 0, 0),2);
+    al_draw_line(385, 95, 395, 85, al_map_rgb(0, 0, 0),2);
+    al_draw_line(405, 85, 415, 95, al_map_rgb(0, 0, 0),2);
+    al_draw_line(405, 95, 415, 85, al_map_rgb(0, 0, 0),2);
+    al_draw_line(385, 115, 415, 115, al_map_rgb(0, 0, 0),4);
     al_flip_display();
     al_rest(5);
     al_destroy_display(display);
 }
+
 
 int jeu(int NombreDeVie, Mot mot, int vie, int position) {
     int nombreLettre = 0;
@@ -178,6 +176,7 @@ int jeu(int NombreDeVie, Mot mot, int vie, int position) {
                 vie -= 1;
 
                 if (vie == 0) {
+                    //imagePendu();
                     printf("you are a looser!\n");
                     printf("le mot etait %s !!!\n", mot.mot);
                     return 0;
@@ -213,4 +212,3 @@ int jeu(int NombreDeVie, Mot mot, int vie, int position) {
 
     return 0;
 };
-*/
